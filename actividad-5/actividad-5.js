5e373a71a4efd1c129e01fd0ecf4abc2db462353
Ramon Penilla <ramonpenilla@gmail.com>
2022-03-14 18:43:23 -0600

    main

--- actividad-5/actividad-5.js
+++ actividad-5/actividad-5.js
@@ -1,13 +1,16 @@
 // ejercicio 1
-var arreglo = [1, 4, 6, 10, 22, 55, 46, 2, 5, 0]
-for (var i = 3; i < arreglo.length; i++){
-    console.log(arreglo[i])
+var arreglo = [1, 4, 6, 10, 22, 55, 46, 2, 5, 0];
+for (var i = 0; i < arreglo.length; i++) {
+  if (arreglo[i] > 3) {
+    console.log(arreglo[i]);
+  }
+}
 
 //ejercicio 2
-    var arreglo = []
-    var i = 0;
+var arreglo = [];
+var i = 0;
 
-    while (i < 6) {
-        arreglo.push(i);
-    i++;
+while (i < 6) {
+  arreglo.push(i);
+  i++;
 }
