a636a3f7ae4f1c670cbe6b93793d795be69ccd20
Ramon Penilla <ramonpenilla@gmail.com>
2022-03-17 20:47:49 -0600

    main

--- actvidad-9.js/actividad-9.js
+++ actvidad-9.js/actividad-9.js
@@ -38,9 +38,40 @@ console.log(student.hasOwnProperty('name'));
 
 // Escriba un programa de JavaScript para eliminar la propiedad rollno del siguiente objeto.
 // Imprima también el objeto antes o después de eliminar la propiedad.
-
+/*
 var student = {
   name: "David Rayy",
   sclass: "VI",
   rollno: 12,
 };
+
+delete student.rollno;
+console.log(student);
+*/
+
+//Escriba un programa para modificar el valor de la segunda propiedad del siguiente objeto:
+//La nueva edad debe ser 35.
+/*
+var student = {
+  name: "David Rayy",
+  age: 34,
+  job: "engineer",
+  hobbies: ["Sky", "football", "walking my dog"],
+};
+
+student.age = "35";
+console.log(student);
+*/
+
+// Escriba un programa para añadir una nueva propiedad al siguiente objeto:
+// La nueva propiedad debe ser: pet y agregue un valor, ejemplo: cat, dog.
+
+var student = {
+  name: "David Rayy",
+  age: 34,
+  job: "engineer",
+  hobbies: ["Sky", "football", "walking my dog"],
+};
+student.pet = ["cat", "dog"];
+console.log(student);
+
